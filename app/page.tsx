93091c30bb09abdf248dd77aa077551e782496e1
miapiar <meri.lacouturiere@gmail.com>
2024-10-01 00:45:35 +0100

    added table component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 import DashboardCard from '@/components/dashboard/DashboardCard';
-import { Newspaper } from 'lucide-react';
+import PostTable from '@/components/posts/PostTable';
 
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col md:flex-row" justify-between gap-5 mb-5>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
         <DashboardCard
           title="Posts"
           icon={<Newspaper className="text-slate-500" size={72} />}
           count={100}
         />
+        <DashboardCard
+          title="Categories"
+          icon={<Folder className="text-slate-500" size={72} />}
+          count={12}
+        />
+        <DashboardCard
+          title="Users"
+          icon={<User className="text-slate-500" size={72} />}
+          count={750}
+        />
+        <DashboardCard
+          title="Comments"
+          icon={<MessageCircle className="text-slate-500" size={72} />}
+          count={1200}
+        />
       </div>
+      <PostTable title="Latest Posts" limit={5} />
     </div>
   );
 }
--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from '@/components/ui/card';
-import { Newspaper, LucideIcon } from 'lucide-react';
+import { LucideIcon } from 'lucide-react';
 
 interface dashboardCardProps {
   title: string;
